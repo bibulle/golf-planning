1b291a39a942cf04502afc10f0e2a5d293a85557
m341772 <eric.martin@airfrance.fr>
2022-01-14 15:48:11 +0100

    feat: add logger level from env

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, LogLevel } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
 import * as passport from 'passport';
@@ -13,8 +13,29 @@ import { AppModule } from './app/app.module';
 import { LoggingInterceptor } from './app/interceptors/logging.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let logger_levels: LogLevel[] = ['error', 'warn', 'log'];
+  if (process.env.LOG_LEVEL) {
+    switch (process.env.LOG_LEVEL.toUpperCase()) {
+      case 'VERBOSE':
+        logger_levels = ['error', 'warn', 'log', 'debug', 'verbose'];
+        break;
+      case 'DEBUG':
+        logger_levels = ['error', 'warn', 'log', 'debug'];
+        break;
+      case 'LOG':
+        logger_levels = ['error', 'warn', 'log'];
+        break;
+      default:
+        logger_levels = ['error', 'warn'];
+        break;
+    }
+  }
 
+  const app = await NestFactory.create(AppModule, {
+    logger: logger_levels,
+  });
+
+  console.log(process.env.LOG_LEVEL);
   app.useGlobalInterceptors(new LoggingInterceptor());
 
   // add websocket
