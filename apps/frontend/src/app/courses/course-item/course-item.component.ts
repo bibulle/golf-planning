ada72d73fe8fe4c3a4f041e7b17041f4cb670f2e
m341772 <eric.martin@airfrance.fr>
2022-01-19 12:41:56 +0100

    fix: remove an unused import

--- apps/frontend/src/app/courses/course-item/course-item.component.ts
+++ apps/frontend/src/app/courses/course-item/course-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Course, User } from '@golf-planning/api-interfaces';
 
 @Component({
